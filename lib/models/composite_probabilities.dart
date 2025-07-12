2385c8194b0ef631587752d4bb6e26ffac156950
Jonas Mayer <mayerr.jonas@gmail.com>
2025-07-12 21:12:43 +0200

    migrate the other composite methods

--- lib/models/composite_probabilities.dart
+++ lib/models/composite_probabilities.dart
@@ -41,4 +41,86 @@ class CompositeProbabilities {
 
     return riskMatrix[a][d];
   }
+
+  /// Returns the probability to win an attack with [a] attackers against [d] defenders 
+  /// and have exactly [aLeft] attackers left afterward.
+  double pPreciseWin(int a, int d, int aLeft) {
+    if (a < 1) {
+      throw ArgumentError('Number of attackers must be at least 1, got: $a');
+    }
+    if (d < 1) {
+      throw ArgumentError('Number of defenders must be at least 1, got: $d');
+    }
+    if (aLeft < 0 || aLeft > a) {
+      throw ArgumentError('Attackers left must be between 0 and $a, got: $aLeft');
+    }
+
+    final winMatrix = List.generate(
+      a + 1, 
+      (i) => List.filled(d + 1, 0.0)
+    );
+
+    for (int i = 0; i <= a; i++) {
+      for (int j = 0; j <= d; j++) {
+        double y;
+        if (i == 0) {
+          y = 0.0;
+        } else if (i < aLeft) {
+          y = 0.0;
+        } else if (i > aLeft && j == 0) {
+          y = 0.0;
+        } else if (i == aLeft && j == 0) {
+          y = 1.0;
+        } else {
+          final singleWinProb = _basicProbs.pSingleWin(i, j);
+          y = singleWinProb * winMatrix[i][j - 1] +
+              (1 - singleWinProb) * winMatrix[i - 1][j];
+        }
+        winMatrix[i][j] = y;
+      }
+    }
+
+    return winMatrix[a][d];
+  }
+
+  /// Returns the probability to lose an attack with [a] attackers against [d] defenders 
+  /// with the condition that there are exactly [dLeft] defenders left afterward.
+  double pPreciseLoss(int a, int d, int dLeft) {
+    if (a < 1) {
+      throw ArgumentError('Number of attackers must be at least 1, got: $a');
+    }
+    if (d < 1) {
+      throw ArgumentError('Number of defenders must be at least 1, got: $d');
+    }
+    if (dLeft < 1 || dLeft > d) {
+      throw ArgumentError('Defenders left must be between 1 and $d, got: $dLeft');
+    }
+
+    final lossMatrix = List.generate(
+      a + 1, 
+      (i) => List.filled(d + 1, 0.0)
+    );
+
+    for (int i = 0; i <= a; i++) {
+      for (int j = 0; j <= d; j++) {
+        double y;
+        if (j == 0) {
+          y = 0.0;
+        } else if (j < dLeft) {
+          y = 0.0;
+        } else if (i == 0 && j > dLeft) {
+          y = 0.0;
+        } else if (j == dLeft && i == 0) {
+          y = 1.0;
+        } else {
+          final singleWinProb = _basicProbs.pSingleWin(i, j);
+          y = singleWinProb * lossMatrix[i][j - 1] +
+              (1 - singleWinProb) * lossMatrix[i - 1][j];
+        }
+        lossMatrix[i][j] = y;
+      }
+    }
+
+    return lossMatrix[a][d];
+  }
 }
--- test/composite_probabilities_test.dart
+++ test/composite_probabilities_test.dart
@@ -162,5 +162,127 @@ void main() {
                reason: '1v1 composite should equal single roll probability');
       });
     });
+
+    group('Precise Win Probabilities', () {
+      test('pPreciseWin input validation', () {
+        expect(() => compositeProbs.pPreciseWin(0, 5, 2), 
+               throwsA(isA<ArgumentError>()));
+        expect(() => compositeProbs.pPreciseWin(5, 0, 2), 
+               throwsA(isA<ArgumentError>()));
+        expect(() => compositeProbs.pPreciseWin(5, 3, -1), 
+               throwsA(isA<ArgumentError>()));
+        expect(() => compositeProbs.pPreciseWin(5, 3, 6), 
+               throwsA(isA<ArgumentError>()));
+      });
+
+      test('pPreciseWin returns valid probabilities', () {
+        final testCases = [
+          [3, 2, 1], [5, 3, 2], [4, 4, 0], [6, 2, 3]
+        ];
+
+        for (final testCase in testCases) {
+          final a = testCase[0];
+          final d = testCase[1];
+          final aLeft = testCase[2];
+          
+          final prob = compositeProbs.pPreciseWin(a, d, aLeft);
+          
+          expect(prob, greaterThanOrEqualTo(0.0), 
+                 reason: 'pPreciseWin($a, $d, $aLeft) should be >= 0');
+          expect(prob, lessThanOrEqualTo(1.0), 
+                 reason: 'pPreciseWin($a, $d, $aLeft) should be <= 1');
+        }
+      });
+
+      test('sum of all precise win probabilities equals overall win probability', () {
+        const a = 5;
+        const d = 3;
+        
+        final overallWinProb = compositeProbs.pMatrix(a, d);
+        
+        double sumPreciseWins = 0.0;
+        for (int aLeft = 0; aLeft <= a; aLeft++) {
+          sumPreciseWins += compositeProbs.pPreciseWin(a, d, aLeft);
+        }
+        
+        expect(sumPreciseWins, closeTo(overallWinProb, 1e-10),
+               reason: 'Sum of precise win probabilities should equal overall win probability');
+      });
+    });
+
+    group('Precise Loss Probabilities', () {
+      test('pPreciseLoss input validation', () {
+        expect(() => compositeProbs.pPreciseLoss(0, 5, 2), 
+               throwsA(isA<ArgumentError>()));
+        expect(() => compositeProbs.pPreciseLoss(5, 0, 2), 
+               throwsA(isA<ArgumentError>()));
+        expect(() => compositeProbs.pPreciseLoss(5, 3, 0), 
+               throwsA(isA<ArgumentError>()));
+        expect(() => compositeProbs.pPreciseLoss(5, 3, 4), 
+               throwsA(isA<ArgumentError>()));
+      });
+
+      test('pPreciseLoss returns valid probabilities', () {
+        final testCases = [
+          [3, 4, 2], [2, 5, 3], [4, 3, 1], [6, 4, 2]
+        ];
+
+        for (final testCase in testCases) {
+          final a = testCase[0];
+          final d = testCase[1];
+          final dLeft = testCase[2];
+          
+          final prob = compositeProbs.pPreciseLoss(a, d, dLeft);
+          
+          expect(prob, greaterThanOrEqualTo(0.0), 
+                 reason: 'pPreciseLoss($a, $d, $dLeft) should be >= 0');
+          expect(prob, lessThanOrEqualTo(1.0), 
+                 reason: 'pPreciseLoss($a, $d, $dLeft) should be <= 1');
+        }
+      });
+
+      test('sum of all precise loss probabilities equals overall loss probability', () {
+        const a = 4;
+        const d = 6;
+        
+        final overallLossProb = 1.0 - compositeProbs.pMatrix(a, d);
+        
+        double sumPreciseLosses = 0.0;
+        for (int dLeft = 1; dLeft <= d; dLeft++) {
+          sumPreciseLosses += compositeProbs.pPreciseLoss(a, d, dLeft);
+        }
+        
+        expect(sumPreciseLosses, closeTo(overallLossProb, 1e-10),
+               reason: 'Sum of precise loss probabilities should equal overall loss probability');
+      });
+    });
+
+    group('Complete Probability Coverage', () {
+      test('sum of all win and loss probabilities equals 1.0', () {
+        final testCases = [
+          [3, 3], [5, 4], [7, 6], [4, 8]
+        ];
+
+        for (final testCase in testCases) {
+          final a = testCase[0];
+          final d = testCase[1];
+          
+          double sumWinProbs = 0.0;
+          for (int aLeft = 0; aLeft <= a; aLeft++) {
+            sumWinProbs += compositeProbs.pPreciseWin(a, d, aLeft);
+          }
+          
+          double sumLossProbs = 0.0;
+          for (int dLeft = 1; dLeft <= d; dLeft++) {
+            sumLossProbs += compositeProbs.pPreciseLoss(a, d, dLeft);
+          }
+          
+          final totalProb = sumWinProbs + sumLossProbs;
+          
+          expect(totalProb, closeTo(1.0, 1e-10),
+                 reason: 'Sum of all precise probabilities for ($a, $d) should equal 1.0');
+        }
+      });
+    });
   });
 }
