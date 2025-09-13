df7fc80cf51f9410c47a78e25b3d008d3009317e
Jonas Mayer <mayerr.jonas@gmail.com>
2025-09-13 12:24:37 +0200

    make factorial mehtod recursive

--- lib/models/composite_probabilities.dart
+++ lib/models/composite_probabilities.dart
@@ -175,10 +175,6 @@ class CompositeProbabilities {
   double _factorial(int n) {
     if (n < 0) return 0;
     if (n <= 1) return 1;
-    double result = 1;
-    for (int i = 2; i <= n; i++) {
-      result *= i;
-    }
-    return result;
+    return n * _factorial(n - 1);
   }
 }
